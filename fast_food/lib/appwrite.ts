cc2d581fcd96ed01cf6436de485924645da23326
AnnaDzig <adzyhota@protonmail.com>
2025-09-01 23:25:33 +0200

    setap appwrite

--- fast_food/lib/appwrite.ts
+++ fast_food/lib/appwrite.ts
@@ -4,4 +4,5 @@ export const appwriteConfig = {
   endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
   platform: "com.cv.foodordering",
   projectId: process.env.EXPO_PUBLIC_APPWRITE_ID,
+  databaseId: "68b5802d0037a61a23e7",
 };
