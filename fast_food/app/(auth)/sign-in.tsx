ffbf1ab0242721f8aedb0d37caf025ce7957930b
AnnaDzig <adzyhota@protonmail.com>
2025-09-06 23:46:39 +0200

    sign in update

--- fast_food/app/(auth)/sign-in.tsx
+++ fast_food/app/(auth)/sign-in.tsx
@@ -1,12 +1,37 @@
-import { View, Text, Button } from "react-native";
+import { View, Text, Button, Alert } from "react-native";
 import React, { useState } from "react";
 import { Link, router } from "expo-router";
 import CustomInput from "@/components/CustomInput";
 import CustomButton from "@/components/CustomButton";
+import { signIn } from "@/lib/appwrite";
+import * as Sentry from "@sentry/react-native";
 
 const SignIn = () => {
   const [isSubmitting, setIsSubmitting] = useState(false);
   const [form, setForm] = useState({ email: "", password: "" });
+
+  const submit = async () => {
+    const { email, password } = form;
+
+    if (!email || !password)
+      return Alert.alert(
+        "Error",
+        "Please enter valid email address & password."
+      );
+
+    setIsSubmitting(true);
+
+    try {
+      await SignIn({ email, password });
+
+      router.replace("/");
+    } catch (error: any) {
+      Alert.alert("Error", error.message);
+      Sentry.captureEvent(error);
+    } finally {
+      setIsSubmitting(false);
+    }
+  };
   return (
     <View className="gap-10 bg-white rounded-lg p-5 mt-5">
       <Text>signIn</Text>
