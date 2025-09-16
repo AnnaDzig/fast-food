b33009ca4bb6e647b61060a9c51a72a9171fbd8e
AnnaDzig <adzyhota@protonmail.com>
2025-09-16 22:52:00 +0200

    appwrite

--- fast_food/lib/appwrite.ts
+++ fast_food/lib/appwrite.ts
@@ -1,9 +1,37 @@
-import { Platform } from "react-native";
+import SignIn from "@/app/(auth)/sign-in";
+import { CreateUserParams } from "@/type";
+import { Account, Avatars, Client, Databases, ID } from "react-native-appwrite";
 
 export const appwriteConfig = {
-  endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
+  endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT!,
   platform: "com.cv.foodordering",
-  projectId: process.env.EXPO_PUBLIC_APPWRITE_ID,
+  projectId: process.env.EXPO_PUBLIC_APPWRITE_ID!,
   databaseId: "68b5802d0037a61a23e7",
   userTableId: "user",
+};
+
+export const client = new Client();
+
+client
+  .setEndpoint(appwriteConfig.endpoint)
+  .setProject(appwriteConfig.projectId)
+  .setPlatform(appwriteConfig.platform);
+
+export const account = new Account(client);
+export const databases = new Databases(client);
+const avatars = new Avatars(client);
+
+export const createUser = async ({
+  email,
+  password,
+  name,
+}: CreateUserParams) => {
+  try {
+    const newAccount = await account.create(ID.unique(), email, password, name);
+    if (!newAccount) throw Error;
+    await SignIn({ email, password });
+    const avatarUrl = avatars.getInitialsURL(name);
+  } catch (e) {
+    throw new Error(e as string);
+  }
 };
